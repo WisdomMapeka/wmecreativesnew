eb5d66156e7424462b062c54b252b0989c764619
wisdommapeka <wisdommapeka@gmail.com>
2022-04-11 17:32:41 +0200

    website icon added

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -3,3 +3,6 @@ from . models import *
 def google_analytics_processor(request):
     analyticscode = AnalyticsCode.objects.all().first()            
     return {'analyticscode': analyticscode}
+
+
+
